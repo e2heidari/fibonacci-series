f9509e2e0aef9b126318db62914252b1a48e2718
Ehsan <Ehsan.heydari@gmail.com>
2020-06-08 02:04:48 -0700

    add input and  complet fibonacci formula

--- fibonacci-series.js
+++ fibonacci-series.js
@@ -2,9 +2,14 @@ const fibonacci = function () {
     let x = 0;
     let y = 1;
     let z = 0;
+    let counterValue = document.getElementById("counter").value;
+    counterValue = parseInt(counter);
     for (let counter = 2; counter < 10; counter++) {
+
         z = x + y;
         x = y;
-        x = z;
+        y = z;
     }
+    Answer = z
+    document.getElementById("answer").innerHTML = Answer;
 }
